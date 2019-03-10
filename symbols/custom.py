6ca5450398e3d41bd91abb8bba20bee95652cfde
hongyigeng <120948741@qq.com>
2019-03-10 15:29:04 +0800

    updated showSymbolShape.py adjusting custom network for better performance

--- symbols/custom.py
+++ symbols/custom.py
@@ -3,44 +3,50 @@ import mxnet as mx
 def get_symbol(num_classes=10):
     data = mx.symbol.Variable('data')
 
-    conv1 = mx.symbol.Convolution(data=data, kernel=(8,8), stride=(4,4),num_filter=96)
+    conv1 = mx.symbol.Convolution(data=data, kernel=(8, 8), stride=(4, 4), num_filter=256)
     bn1 = mx.symbol.BatchNorm(data=conv1)
     relu1 = mx.symbol.Activation(data=bn1, act_type="relu")
-    pool1 = mx.symbol.Pooling(data=relu1, pool_type="max", kernel=(3,3), stride=(2,2))
-    #dropout1 = mx.symbol.Dropout(data=pool1, p=0.5)
+    pool1 = mx.symbol.Pooling(data=relu1, pool_type="max", kernel=(3, 3), stride=(2, 2))
+    dropout1 = mx.symbol.Dropout(data=pool1, p=0.5)
 
-    conv2 = mx.symbol.Convolution(data=pool1, kernel=(3,3), pad=(2, 2), num_filter=256)
+    conv2 = mx.symbol.Convolution(data=dropout1, kernel=(3, 3), pad=(2, 2), num_filter=256)
     bn2 = mx.symbol.BatchNorm(data=conv2)
     relu2 = mx.symbol.Activation(data=bn2, act_type="relu")
-    pool2 = mx.symbol.Pooling(data=relu2, pool_type="max", kernel=(3,3), stride=(2,2))
-    #dropout2 = mx.symbol.Dropout(data=pool2, p=0.5)
+    pool2 = mx.symbol.Pooling(data=relu2, pool_type="max", kernel=(3, 3), stride=(2, 2))
+    dropout2 = mx.symbol.Dropout(data=pool2, p=0.5)
 
-    conv3 = mx.symbol.Convolution(data=pool2, kernel=(3, 3), pad=(2, 2), num_filter=256)
+    conv3 = mx.symbol.Convolution(data=dropout2, kernel=(3, 3), pad=(1, 1), num_filter=256)
     bn3 = mx.symbol.BatchNorm(data=conv3)
     relu3 = mx.symbol.Activation(data=bn3, act_type="relu")
-    pool3 = mx.symbol.Pooling(data=relu3, pool_type="max", kernel=(3, 3), stride=(2, 2))
-    #dropout3 = mx.symbol.Dropout(data=pool3, p=0.5)
-
-    conv4 = mx.symbol.Convolution(data=pool3, kernel=(3, 3), pad=(2, 2), num_filter=96)
-    bn4 = mx.symbol.BatchNorm(data=conv4)
-    relu4 = mx.symbol.Activation(data=bn4, act_type="relu")
-    pool4 = mx.symbol.Pooling(data=relu4, pool_type="max", kernel=(3, 3), stride=(2, 2))
-    #dropout4 = mx.symbol.Dropout(data=pool4, p=0.5)
-
-    flatten = mx.symbol.Flatten(data=pool4)
-    fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=1000)
-    relu3 = mx.symbol.Activation(data=fc1, act_type="relu")
+    # pool3 = mx.symbol.Pooling(data=relu3, pool_type="max", kernel=(3, 3), stride=(2, 2))
     dropout3 = mx.symbol.Dropout(data=relu3, p=0.5)
 
-    fc2 = mx.symbol.FullyConnected(data=dropout3, num_hidden=1000)
-    relu4 = mx.symbol.Activation(data=fc2, act_type="relu")
+    conv4 = mx.symbol.Convolution(data=dropout3, kernel=(3, 3), pad=(1, 1), num_filter=256)
+    bn4 = mx.symbol.BatchNorm(data=conv4)
+    relu4 = mx.symbol.Activation(data=bn4, act_type="relu")
+    # pool4 = mx.symbol.Pooling(data=relu4, pool_type="max", kernel=(3, 3), stride=(2, 2))
     dropout4 = mx.symbol.Dropout(data=relu4, p=0.5)
+
+    conv5 = mx.symbol.Convolution(data=dropout4, kernel=(3, 3), pad=(1, 1), num_filter=128)
+    bn5 = mx.symbol.BatchNorm(data=conv5)
+    relu5 = mx.symbol.Activation(data=bn5, act_type="relu")
+    pool5 = mx.symbol.Pooling(data=relu5, pool_type="max", kernel=(3, 3), stride=(2, 2))
+    dropout5 = mx.symbol.Dropout(data=pool5, p=0.5)
+
+    flatten = mx.symbol.Flatten(data=dropout5)
+    fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=1000)
+    relu6 = mx.symbol.Activation(data=fc1, act_type="relu")
+    dropout6 = mx.symbol.Dropout(data=relu6, p=0.5)
+
+    fc2 = mx.symbol.FullyConnected(data=dropout6, num_hidden=1000)
+    relu7 = mx.symbol.Activation(data=fc2, act_type="relu")
+    dropout7 = mx.symbol.Dropout(data=relu7, p=0.5)
 
     # fc3 = mx.symbol.FullyConnected(data=dropout4, num_hidden=500)
     # relu5 = mx.symbol.Activation(data=fc3, act_type="relu")
     # dropout5 = mx.symbol.Dropout(data=relu5, p=0.5)
 
-    fc2 = mx.symbol.FullyConnected(data=dropout4, num_hidden=num_classes)
+    fc3 = mx.symbol.FullyConnected(data=dropout7, num_hidden=num_classes)
 
-    custom = mx.symbol.SoftmaxOutput(data=fc2, name='softmax')
+    custom = mx.symbol.SoftmaxOutput(data=fc3, name='softmax')
     return custom
--- trainings/trainOnResizedObjects.py
+++ trainings/trainOnResizedObjects.py
@@ -5,7 +5,7 @@ import logging
 import time
 from importlib import import_module
 
-netName = 'custom'   #the symbol file name in symbols directory
+netName = 'vgg16'   #the symbol file name in symbols directory
 net = import_module('symbols.'+ netName)
 sym = net.get_symbol(num_classes=20)
 mod = mx.mod.Module(sym, context = mx.gpu())
